6b6156941d6942829ef7fb1a22e84254d519cdd2
chen <chen>
2016-05-25 16:24:25 +0800

    day 1

---
+++ test/vss.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*- 
+#sys模块 sys模块包含了与Python解释器和它的环境有关的函数
+import sys
+#subprocess模块 subprocess包中定义有数个创建子进程的函数，这些函数分别以不同的方式创建子进程，所以我们可以根据需要来从中选取一个使用
+import subprocess  
+#os模块，用来执行cmd/shell/linux等命令
+import os  
